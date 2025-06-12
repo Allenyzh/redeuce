40682df4c2ff5b633094d58a501ed5b2b9f5b105
ZHENHAO YANG <49468724+Allenyzh@users.noreply.github.com>
2025-06-12 16:18:53 -0400

    -

--- script.js
+++ script.js
@@ -7,3 +7,14 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
     });
   });
 });
+
+
+$(function() {
+  $("#navbar-placeholder").load("./components/nav.html");
+  $("#footer-placeholder").load("./components/footer.html", function() {
+    // 只有footer加载完成后再设置年份
+    const currentYear = new Date().getFullYear();
+    $(".footer p").html(`Copyright © ${currentYear} - All right reserved`);
+  });
+});
+
